548ccc84db6c940cd2a86ebf49994f2500100be0
Phill Maslo <phillmaslo@gmail.com>
2025-05-18 15:24:48 +0200

    renamed AuthSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from './auth/authSlice';
+import { authReducer } from './auth/Slice';
 import { contactsReducer } from './contacts/slice';
 import { filtersReducer } from './filters/slice';
 
